311a26176295058e8cbb4ff7c611da3243a9e7ab
irmbyrk <irembayrak1@gmail.com>
2020-08-11 20:00:01 +0200

    header styling

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,47 @@ $(document).ready(function(){
     $(this).toggleClass("is-active");
   });
 });
+
+window.onscroll = function() {scrollFunction()};
+
+$(window).scroll(function () {
+if ($(window).scrollTop() >= 101) {
+$('#navbar').css('background','rgba(255, 255, 255, 0.4)');
+} else {
+$('#navbar').css('background','#ffedbc');
+}
+});
+
+$(window).scroll(function () {
+if ($(window).scrollTop() >= 101) {
+$('#about-navbar').css('background','rgba(255, 255, 255, 0.4)');
+} else {
+$('#about-navbar').css('background','white');
+}
+});
+
+$(window).scroll(function () {
+if ($(window).scrollTop() >= 101) {
+$('#pati-navbar').css('background','rgba(255, 255, 255, 0.4)');
+} else {
+$('#pati-navbar').css('background','#ee8761');
+}
+});
+
+$(window).scroll(function () {
+if ($(window).scrollTop() >= 101) {
+$('#vocapp-navbar').css('background','rgba(255, 255, 255, 0.4)');
+} else {
+$('#vocapp-navbar').css('background','#F4FAD2');
+}
+});
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("navbar").style.font-weight = "700";
+//     document.getElementById("logo").style.font-weight = "700";
+//   } else {
+//     document.getElementById("navbar").style.font-weight = "700";
+//     document.getElementById("logo").style.font-weight = "700";
+//   }
+// }
