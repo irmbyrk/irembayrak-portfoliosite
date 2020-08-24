75027ee19cf22f3d7d50e188727c71b0a65f7bfc
irmbyrk <irembayrak1@gmail.com>
2020-08-24 16:54:18 +0200

    header onscroll js

--- js/scripts.js
+++ js/scripts.js
@@ -22,28 +22,28 @@ $(document).ready(function() {
 //   });
 // });
 //
-// window.onscroll = function() {
-//   scrollFunction()
-// };
-//
-// function scrollFunction() {
-//   if (window.innerWidth >= 640) {
-//     if (document.body.scrollTop > 101 || document.documentElement.scrollTop > 101) {
-//       document.getElementById("navbar").style.padding = "10px 0 3px";
-//       document.getElementById("navbar").style.boxShadow = "0px 0px 30px -6px rgba(148,148,148,1)";
-//       document.getElementById("logo").style.maxWidth = "60px";
-//       document.getElementById("menuindex").style.fontSize = "16px";
-//       document.getElementById("menuabout").style.fontSize = "16px";
-//       document.getElementById("menucontact").style.fontSize = "16px";
-//       document.getElementById("shrink").style.margin = "0.5em 0";
-//     } else {
-//       document.getElementById("navbar").style.padding = "20px 0 13px";
-//       document.getElementById("navbar").style.boxShadow = "0px 0px 30px -6px rgba(148,148,148,1)";
-//       document.getElementById("logo").style.maxWidth = "75px";
-//       document.getElementById("menuindex").style.fontSize = "18px";
-//       document.getElementById("menuabout").style.fontSize = "18px";
-//       document.getElementById("menucontact").style.fontSize = "18px";
-//       document.getElementById("shrink").style.margin = "1em 0";
-//     }
-//   }
-// }
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (window.innerWidth >= 640) {
+    if (document.body.scrollTop > 101 || document.documentElement.scrollTop > 101) {
+      document.getElementById("navbar").style.padding = "10px 0 3px";
+      document.getElementById("navbar").style.boxShadow = "0px 0px 30px -6px rgba(148,148,148,1)";
+      document.getElementById("logo").style.maxWidth = "60px";
+      document.getElementById("menuindex").style.fontSize = "16px";
+      document.getElementById("menuabout").style.fontSize = "16px";
+      document.getElementById("menucontact").style.fontSize = "16px";
+      document.getElementById("shrink").style.margin = "0.5em 0";
+    } else {
+      document.getElementById("navbar").style.padding = "20px 0 13px";
+      document.getElementById("navbar").style.boxShadow = "0px 0px 30px -6px rgba(148,148,148,1)";
+      document.getElementById("logo").style.maxWidth = "75px";
+      document.getElementById("menuindex").style.fontSize = "18px";
+      document.getElementById("menuabout").style.fontSize = "18px";
+      document.getElementById("menucontact").style.fontSize = "18px";
+      document.getElementById("shrink").style.margin = "1em 0";
+    }
+  }
+}
