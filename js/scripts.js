efe0d8314e6908ad68fcfe6b9a2337c9a706d6d1
irmbyrk <67583484+irmbyrk@users.noreply.github.com>
2021-01-22 21:22:02 +0100

    new website

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ function scrollFunction() {
   if (window.innerWidth >= 640) {
     if (document.body.scrollTop > 101 || document.documentElement.scrollTop > 101) {
       document.getElementById("navbar").style.padding = "10px 0 3px";
-      document.getElementById("navbar").style.boxShadow = "0px 0px 30px -6px rgba(148,148,148,1)";
+      document.getElementById("navbar").style.boxShadow = "0px 0px 10px -6px rgba(148,148,148,1)";
       document.getElementById("logo").style.maxWidth = "60px";
       document.getElementById("menuindex").style.fontSize = "16px";
       document.getElementById("menuabout").style.fontSize = "16px";
@@ -37,7 +37,7 @@ function scrollFunction() {
       document.getElementById("shrink").style.margin = "0.5em 0";
     } else {
       document.getElementById("navbar").style.padding = "20px 0 13px";
-      document.getElementById("navbar").style.boxShadow = "0px 0px 30px -6px rgba(148,148,148,1)";
+      document.getElementById("navbar").style.boxShadow = "0px 0px 1px -6px rgba(148,148,148,1)";
       document.getElementById("logo").style.maxWidth = "75px";
       document.getElementById("menuindex").style.fontSize = "18px";
       document.getElementById("menuabout").style.fontSize = "18px";
